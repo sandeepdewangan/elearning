f91279da5cb52224bf0c73425ea6057636447814
sandeep-ubuntu <sandeepinwork@gmail.com>
2020-09-17 19:22:58 +0530

    added model: Course

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Subject(models.Model):
@@ -7,6 +8,21 @@ class Subject(models.Model):
 
     class Meta:
         ordering = ['title']
+    
+    def __str__(self):
+        return self.title
+
+
+class Course(models.Model):
+    owner = models.ForeignKey(User, related_name='courses_created', on_delete=models.CASCADE)
+    subject = models.ForeignKey(Subject, related_name='courses', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    overview = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
     
     def __str__(self):
         return self.title
